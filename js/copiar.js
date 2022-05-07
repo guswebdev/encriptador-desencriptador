54228d1e2927e4efaf9f05d261c44517609eb328
Gustavo Miño <gus.desarrollo.web@gmail.com>
2022-05-07 16:55:40 -0300

    Crear el alerta del copiado con exito

--- js/copiar.js
+++ js/copiar.js
@@ -6,7 +6,10 @@ export function copiar() {
             e.preventDefault();
             let textarea = d.querySelector('.output-textarea').value;
             navigator.clipboard.writeText(textarea);
-
+            d.querySelector('.copiado-exito').classList.add('show')
+            setTimeout(() => {
+                d.querySelector('.copiado-exito').classList.remove('show')
+            }, 2000);
         }
     })
 }
