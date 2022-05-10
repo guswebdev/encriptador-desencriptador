f6d538aa98a33d49d6e7c5778317a64ffb515d27
Gustavo Miño <gus.desarrollo.web@gmail.com>
2022-05-09 22:05:32 -0300

    Refactorizar la funcionalidad de encriptar

--- index.js
+++ index.js
@@ -4,13 +4,26 @@ import { desencriptar } from "./js/desencriptar.js";
 import { encriptar } from "./js/encriptar.js";
 
 const d = document;
+let keys;
 
 const inicio = () => {
-  const keys = capturarLlaves();
-  encriptar(keys);
-  desencriptar(keys);
-  copiar();
+  keys = capturarLlaves();
+  //encriptar(keys);
+  //desencriptar(keys);
+  //copiar();
 }
 
 
 d.addEventListener('DOMContentLoaded', inicio)
+
+const click = (e) => {
+
+  if (e.target.matches(".btn-encriptar")){
+    encriptar(e,keys);
+  }
+
+  desencriptar(keys);
+  copiar();
+}
+
+d.addEventListener('click', click)
--- js/encriptar.js
+++ js/encriptar.js
@@ -1,40 +1,37 @@
 const d = document;
 
-export function encriptar(keys) {
-  d.addEventListener("click", (e) => {
-    if (e.target.matches(".btn-encriptar")) {
-      e.preventDefault();
-      let newArr = [];
-      console.log(keys)
-      //let keys = ["ai", "enter", "imes", "ober", "ufat"];
-      let cadena = d.querySelector(".textarea").value;
-      let regExp = /[A-Z\u00C0-\u017F]/g;
-      if (!regExp.test(cadena)) {
-        let array = cadena.toLowerCase().split("");
-        array.forEach((el) => {
-          if (el === "a") {
-            newArr.push(keys[0]);
-          } else if (el === "e") {
-            newArr.push(keys[1]);
-          } else if (el === "i") {
-            newArr.push(keys[2]);
-          } else if (el === "o") {
-            newArr.push(keys[3]);
-          } else if (el === "u") {
-            newArr.push(keys[4]);
-          } else {
-            newArr.push(el);
-          }
-        });
-        let result = newArr.join("");
-        d.querySelector(".output-message").classList.remove("hide");
-        d.querySelector(".output-init").classList.add("hide");
-        d.querySelector(".output-textarea").value = `${result}`;
-        d.querySelector('.info-text').classList.remove('color-red');
-        d.querySelector(".form").reset();
-      } else {
-        d.querySelector('.info-text').classList.add('color-red');
-      }
+export function encriptar(e,keys) {
+  e.preventDefault();
+    let newArr = [];
+    console.log(keys)
+    //let keys = ["ai", "enter", "imes", "ober", "ufat"];
+    let cadena = d.querySelector(".textarea").value;
+    let regExp = /[A-Z\u00C0-\u017F]/g;
+    if (!regExp.test(cadena)) {
+      let array = cadena.toLowerCase().split("");
+      array.forEach((el) => {
+        if (el === "a") {
+          newArr.push(keys[0]);
+        } else if (el === "e") {
+          newArr.push(keys[1]);
+        } else if (el === "i") {
+          newArr.push(keys[2]);
+        } else if (el === "o") {
+          newArr.push(keys[3]);
+        } else if (el === "u") {
+          newArr.push(keys[4]);
+        } else {
+          newArr.push(el);
+        }
+      });
+      let result = newArr.join("");
+      console.log(result)
+      d.querySelector(".output-message").classList.remove("hide");
+      d.querySelector(".output-init").classList.add("hide");
+      d.querySelector(".output-textarea").value = `${result}`;
+      d.querySelector('.info-text').classList.remove('color-red');
+      d.querySelector(".form").reset();
+    } else {
+      d.querySelector('.info-text').classList.add('color-red');
     }
-  });
 }
